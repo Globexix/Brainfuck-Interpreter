4bb3d5331f0adc71b5b6d6510595db8aad9761af
Globex <globexmc@gmail.com>
2026-03-16 00:38:45 +0100

    feat: add brainfuck program and memory buffer

--- brainfuck.s
+++ brainfuck.s
@@ -1,6 +1,14 @@
 .intel_syntax noprefix
 .global _start
 
+.section .rodata
+    # Hello World!
+    code: .ascii "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."
+    .byte 0
+
+.section .bss
+    buffer: .skip 30000
+
 .section .text
 _start: 
     mov rax, 60
